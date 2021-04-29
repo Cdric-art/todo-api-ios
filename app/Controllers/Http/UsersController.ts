0007ea9d2fb0733dad4834240ddeb91abb382414
Cdric_art <cdric.art@gmail.com>
2021-04-29 16:24:36 +0200

    Feat: User model, migration and controller

---
+++ app/Controllers/Http/UsersController.ts
@@ -0,0 +1,25 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import UserValidator from 'App/Validators/UserValidator'
+import User from 'App/Models/User'
+
+export default class UsersController {
+	public async register({ request, response }: HttpContextContract) {
+		const payload = await request.validate(UserValidator)
+		const user = await User.create(payload)
+
+		return response.ok(user)
+	}
+
+	public async login({ auth, request }: HttpContextContract) {
+		const { pseudo, password } = request.only(['pseudo', 'password'])
+
+		await auth.attempt(pseudo, password)
+
+		return auth.user
+	}
+
+	public async logout({ auth, response }: HttpContextContract) {
+		await auth.logout()
+		return response.noContent()
+	}
+}
--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,34 +1,37 @@
 import { DateTime } from 'luxon'
 import Hash from '@ioc:Adonis/Core/Hash'
-import {
-  column,
-  beforeSave,
-  BaseModel,
-} from '@ioc:Adonis/Lucid/Orm'
+import { column, beforeSave, BaseModel, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
+import Todo from 'App/Models/Todo'
 
 export default class User extends BaseModel {
-  @column({ isPrimary: true })
-  public id: number
+	@column({ isPrimary: true })
+	public id: number
 
-  @column()
-  public pseudo: string
+	@column()
+	public pseudo: string
 
-  @column({ serializeAs: null })
-  public password: string
+	@column({ serializeAs: null })
+	public password: string
 
-  @column()
-  public rememberMeToken?: string
+	@column()
+	public email: string
 
-  @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+	@column()
+	public rememberMeToken?: string
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+	@column.dateTime({ autoCreate: true })
+	public createdAt: DateTime
 
-  @beforeSave()
-  public static async hashPassword (user: User) {
-    if (user.$dirty.password) {
-      user.password = await Hash.make(user.password)
-    }
-  }
+	@column.dateTime({ autoCreate: true, autoUpdate: true })
+	public updatedAt: DateTime
+
+	@hasMany(() => Todo)
+	public todos: HasMany<typeof Todo>
+
+	@beforeSave()
+	public static async hashPassword(user: User) {
+		if (user.$dirty.password) {
+			user.password = await Hash.make(user.password)
+		}
+	}
 }
